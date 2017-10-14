3f751b0cb19d76928609b2b9dad26dbd157cb6ee
unknown <CampusUser@CSUFLAPTOP-97.WORKGROUP>
2017-10-13 18:06:02 -0700

    Profile Pic and Blog Pics code

---
+++ public/scripts/blogpic.js
@@ -0,0 +1,60 @@
+var files = [];
+
+$('.alert-success').hide();
+
+var uploadFiles = function() {
+
+  var fd = new FormData()
+  for (var i in files) {
+    fd.append("uploadedFile", files[i])
+  }
+  var uniqueFilename = "true";
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/imagesupload?subdir=blogpics&uniqueFilename=' + uniqueFilename);
+  xhr.onload = function() {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+    if (this.status < 300) {
+      dpd.users.me(function(me) {
+        var data = {};
+        data["username"] = me.username;
+        data["Blogpost"] = "Sample Testing Blog";
+        data["Timestamp"] =  Date.now();
+        data["blogimg"] = response[0].filename;
+        dpd.blog.post(data, function(result, err) {
+          if (err) return console.log(err);
+          console.log(result, result.id);
+          var img = document.querySelector('#img_profile');
+          dpd.blog.get(result.id, function(result) {
+            console.log(result);
+            img.src = "http://localhost:2403//imagesupload/blogpics/" + result["blogimg"];
+          });
+        });
+        });
+    } else {
+      alert(response.message);
+    }
+  };
+  xhr.onerror = function(err) {
+    alert("Error: ", err);
+  }
+  xhr.send(fd);
+
+};
+
+var setFiles = function(element) {
+  console.log('files:', element.files);
+  // Turn the FileList object into an Array
+  files = [];
+  for (var i = 0; i < element.files.length; i++) {
+    files.push(element.files[i]);
+  }
+};
+
+var deleteFile = function(element, id) {
+  $(element).closest('tr').remove();
+  dpd.upload.del(id, function(data, status) {
+    $('.alert-success').show();
+    $('.alert-success').append("File removed!");
+  })
+}
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -8,6 +8,28 @@ $(document).ready(function() {
 
     // Create a user
     dpd.users.login({
+        username: username,
+        password: password
+      }, function(user, err) {
+        if (err) {
+          alert(err.message || (err.errors && err.errors.title));
+          return;
+        }
+window.location.replace("Profile.html");
+    });
+  }
+
+
+
+
+$('#logout-user').click(logoutUser);
+
+function logoutUser() {
+  var username = $('#login-name').val();
+  var password = $('#login-pass').val();
+
+
+  dpd.users.logout({
       username: username,
       password: password
     }, function(user, err) {
@@ -15,10 +37,8 @@ $(document).ready(function() {
         alert(err.message || (err.errors && err.errors.title));
         return;
       }
-    });
-    window.location.replace("Profile.html");
-    console.log(username);
-    return false;
-  }
+window.location.replace("index.html");
+  });
+}
 
 });
